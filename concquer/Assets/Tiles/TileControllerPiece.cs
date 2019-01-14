3648205466c356f24d40ea3b57e14f3a9cb6a7bd
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2019-01-14 10:37:37 -0800

    some preliminary pieces

---
+++ concquer/Assets/Tiles/TileControllerPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "TileController", menuName = "CustomTiles/TileController")]
+public class TileControllerPiece : Tile
+{
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        //if (go)
+        //{
+        //    var myTileObject = go.GetComponent<TileController>();
+
+        //    if (myTileObject)
+        //    {
+        //        myTileObject.SetParams(position, tilemap, this);
+        //    }
+        //}
+        return true;
+    }
+}
