796a5d937878b97938103d0a593107b8c046f85f
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2019-04-22 20:52:17 -0700

    testing stuff.

---
+++ TestingStansAssets2/Assets/Plugins/StansAssets/CrossPlatform/UltimateMobile/Samples/LocalNotifications/UM_LocalNotificationsExample.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using SA.CrossPlatform.UI;
+using UnityEngine.UI;
+using SA.CrossPlatform.Notifications;
+
+public class UM_LocalNotificationsExample : MonoBehaviour
+{
+#pragma warning disable 649
+    [Header("Unified API Buttons")] 
+    [SerializeField] private Button m_Create5SecNotification = null;
+#pragma warning restore 649
+    
+    private void Start()
+    {
+        var client = UM_NotificationCenter.Client;
+
+        m_Create5SecNotification.onClick.AddListener(() =>
+        {
+            
+            var content = new UM_Notification();
+            
+            content.SetTitle("Title");
+            content.SetBody("Body message");
+            content.SetSmallIconName("myIcon.png");
+            content.SetSoundName("mySound.wav");
+
+            //Get unique request id, if notification will be posted with the same request id old one will be replaced with the new one
+            var requestId = 1; 
+            //2 seconds
+            var trigger = new UM_TimeIntervalNotificationTrigger(2);
+            var request = new UM_NotificationRequest(requestId, content, trigger);
+            client.AddNotificationRequest(request, (result) => {
+                if(result.IsSucceeded) {
+                    UM_DialogsUtility.ShowMessage("Succeeded", "Notification was successfully scheduled.");
+                } else {
+                    UM_DialogsUtility.ShowMessage("Failed", result.Error.FullMessage);
+                }
+            });
+        });
+    }
+
+    public static void SubscribeToTheNotificationEvents()
+    {
+        var client = UM_NotificationCenter.Client;
+        var startRequest = client.LastOpenedNotification;
+        if(startRequest != null) {
+            UM_DialogsUtility.ShowMessage("Launched via Notification", GetNotificationInfo(startRequest));
+            //if this isn't null on your app launch, means user launched your app by clicking on notification icon
+        }
+        
+        client.OnNotificationReceived.AddListener(request => {
+            //Notification was received while app is running
+            UM_DialogsUtility.ShowMessage("Notification Received", GetNotificationInfo(request));
+        });
+        
+        client.OnNotificationClick.AddListener(request => {
+            //User clicked on notification while app is running
+            UM_DialogsUtility.ShowMessage("Restored from background via Notification", GetNotificationInfo(request));
+        });
+    }
+
+    private static string GetNotificationInfo(UM_NotificationRequest request)
+    {
+        return string.Format("request.Identifier: {0} \n request.Content.Title: {1}",
+            request.Identifier,
+            request.Content.Title);
+    }
+}
---
+++ TestingStansAssets2/Assets/Plugins/StansAssets/CrossPlatform/UltimateMobile/Samples/WelcomePage/UM_WelcomeController.cs
@@ -0,0 +1,103 @@
+﻿using SA.CrossPlatform.App;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using SA.iOS.UIKit;
+
+public class UM_WelcomeController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_ButtonsPanel = null;
+    [SerializeField] private GameObject m_FeatureViewport = null;
+    
+    [SerializeField] private Text m_LocaleLabel = null;
+
+    private Scene m_currentlyFeaturedScene;
+
+    private void Start()
+    {
+       SceneManager.sceneLoaded += SceneManagerOnSceneLoaded;
+       var buttons =  m_ButtonsPanel.GetComponentsInChildren<Button>();
+       foreach (var button in buttons)
+       {
+           var currentButton = button;
+           button.onClick.AddListener(() =>
+           {
+               var sceneLink = currentButton.GetComponent<UM_SceneLink>();
+               if (sceneLink != null)
+               {
+                   LoadScene(sceneLink.SceneName);
+               }
+           });
+       }
+
+       InitMainScreenServices();
+    }
+
+    private void InitMainScreenServices()
+    {
+        LocaleInfo();
+        UM_LocalNotificationsExample.SubscribeToTheNotificationEvents();
+    }
+
+    private void LocaleInfo()
+    {
+        var currentLocale = UM_Locale.GetCurrentLocale();
+        m_LocaleLabel.text =  string.Format("<b>Locale Info:</b> " +
+                             "\n CountryCode: {0} " +
+                             "\n LanguageCode: {1}" +
+                             "\n CurrencyCode: {2}" +
+                             "\n CurrencySymbol: {3}",
+            currentLocale.CountryCode,
+            currentLocale.LanguageCode,
+            currentLocale.CurrencyCode,
+            currentLocale.CurrencySymbol);
+    }
+
+    private void SceneManagerOnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        m_currentlyFeaturedScene = scene;
+        foreach (var rootGameObject in scene.GetRootGameObjects())
+        {
+            if (rootGameObject.GetComponent<Canvas>() == null)
+            {
+                Destroy(rootGameObject);
+            }
+            else
+            {
+                var canvasRect = rootGameObject.GetComponent<RectTransform>();
+                rootGameObject.transform.SetParent(m_FeatureViewport.transform);
+                canvasRect.anchorMin = new Vector2(0, 0);
+                canvasRect.anchorMax = new Vector2(1, 1);
+                
+                canvasRect.transform.localScale = Vector3.one;
+                canvasRect.anchoredPosition  = Vector2.zero;
+
+                canvasRect.offsetMin = Vector2.zero;
+                canvasRect.offsetMax = Vector2.zero;
+
+            }
+        }
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        if (sceneName.Equals(m_currentlyFeaturedScene.name))
+        {
+            return;
+        }
+
+        m_FeatureViewport.Clear();
+        if (m_currentlyFeaturedScene.isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(m_currentlyFeaturedScene);
+        }
+       
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+    
+    
+}
+
+
+
